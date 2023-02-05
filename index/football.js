93fb9ad7d158f141d7379f0315fe3a92b623d836
jasonlau <jasonlauxd@gmail.com>
2023-02-05 18:03:24 +0800

    main-layout

---
+++ index/football.js
@@ -0,0 +1,30 @@
+//Football animation --------------------------------------
+let isPressed = false;
+
+let footballX = 0;
+
+document.getElementById("footballImg").addEventListener("click", () => {
+    if (!isPressed) {
+        let footballX = Math.floor(Math.random() * 110) - 55;
+        let footballY = Math.floor(Math.random() * 40) + 80;
+        console.log(footballX);
+        document.documentElement.style.setProperty('--footballY', `-${footballY}px`);
+        document.documentElement.style.setProperty('--footballX', `${footballX}px`);
+
+        document.getElementById("footballImg").classList.add("footballAnimation");
+        isPressed = true;
+
+        setTimeout(() => {
+            document.getElementById("footballImg").classList.remove("footballAnimation");
+            isPressed = false;
+        }, 750);
+    }    
+});
+
+document.getElementById("footballImg").addEventListener("mouseover", () => {
+    document.getElementById("footballImg").style.transform = "scale(1.2, 1.2)";
+});
+
+document.getElementById("footballImg").addEventListener("mouseout", () => {
+    document.getElementById("footballImg").style.transform = "scale(1, 1)";
+});
--- index/index.js
+++ index/index.js
@@ -1,35 +1,22 @@
+//icon roll
+let windowWidth = window.innerWidth;
+document.documentElement.style.setProperty('--rollx1', `${windowWidth/6}px`);
+document.documentElement.style.setProperty('--rollx2', `${windowWidth/6*2.3}px`);
 
-//Football animation --------------------------------------
-let isPressed = false;
+console.log(windowWidth);
 
-let footballX = 0;
-
-document.getElementById("footballImg").addEventListener("click", () => {
-    if (!isPressed) {
-        let footballX = Math.floor(Math.random() * 110) - 55;
-        let footballY = Math.floor(Math.random() * 40) + 80;
-        console.log(footballX);
-        document.documentElement.style.setProperty('--footballY', `-${footballY}px`);
-        document.documentElement.style.setProperty('--footballX', `${footballX}px`);
-
-        document.getElementById("footballImg").classList.add("footballAnimation");
-        isPressed = true;
-
-        setTimeout(() => {
-            document.getElementById("footballImg").classList.remove("footballAnimation");
-            isPressed = false;
-        }, 750);
-    }    
-});
-
-document.getElementById("footballImg").addEventListener("mouseover", () => {
-    document.getElementById("footballImg").style.transform = "scale(1.2, 1.2)";
-});
-
-document.getElementById("footballImg").addEventListener("mouseout", () => {
-    document.getElementById("footballImg").style.transform = "scale(1, 1)";
-});
-
-//
-
-
+let rollDescList = document.getElementsByClassName("icon-desc");
+let rollList = document.getElementsByClassName("icon-circle");
+setTimeout( () => {
+    for (let i = 0; i < rollList.length; i++) {
+        rollList[i].addEventListener("mouseover", () => {
+            rollDescList[i].style.opacity = "1";
+        });
+    }
+    
+    for (let i = 0; i < rollList.length; i++) {
+        rollList[i].addEventListener("mouseout", () => {
+            rollDescList[i].style.opacity = "0";
+        });
+    }
+}, 2000)
